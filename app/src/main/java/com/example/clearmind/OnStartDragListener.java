7a8c695fb8f5d8e743b0f7d06ccb6b8fc60d67bd
yujiaen1999 <673130539@qq.com>
2024-10-06 17:11:08 -0700

    fixed some typo in presurvey

--- app/src/main/java/com/example/clearmind/OnStartDragListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.clearmind;
-
-import androidx.recyclerview.widget.RecyclerView;
-
-public interface OnStartDragListener {
-    void onStartDrag(RecyclerView.ViewHolder viewHolder);
-}
